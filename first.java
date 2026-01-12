7be30ed306d4017946bf3383ed28375a000a032b
Priyanshu-Chauhan-1 <priyanshuchauhan19705@gmail.com>
2026-01-12 14:55:29 +0530

    First Program and Java Literals

---
+++ first.java
@@ -0,0 +1,10 @@
+class first {
+    public static void main(String[] args) {
+        System.out.print("The sum of three numbers is : ");
+        int num1 = 5;
+        int num2 = 6;
+        int num3 = 8;
+        int sum = num1 + num2 + num3;
+        System.out.print(sum);
+    }
+}
---
+++ literals.java
@@ -0,0 +1,32 @@
+public class literals {
+    public static void main(String[] args) {
+        System.out.println("JAVA Literals : ");
+        byte age = 2; // byte literal
+        short age2 = 5; // short literal
+        int age3 = 67; // int literal
+        long age4 = 45555L; // long literal -> l/L in the end of the number
+        float a = 0.5f; // float literal -> f/F in the end of the number
+        double b = 0.5; // BY DEFAULT -> also we can write as 0.5d/ 0.5D
+        char c = 'C'; // character literal
+        boolean val = true; // boolean literal
+        String str = "Hello, This is String Data Type."; // we can define string data type as this
+
+        System.out.print("Byte Data Type : ");
+        System.out.println(age);
+        System.out.print("Short Data Type : ");
+        System.out.println(age2);
+        System.out.print("Int Data Type : ");
+        System.out.println(age3);
+        System.out.print("Long Data Type : ");
+        System.out.println(age4);
+        System.out.print("Float Data Type : ");
+        System.out.println(a);
+        System.out.print("Double Data Type : ");
+        System.out.println(b);
+        System.out.print("Char Data Type : ");
+        System.out.println(c);
+        System.out.print("Boolean Data Type : ");
+        System.out.println(val);
+        System.out.println(str);
+    }
+}
