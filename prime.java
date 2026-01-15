2d41bfb75606b78da4d3e0c992d4edce5895a29e
Priyanshu-Chauhan-1 <priyanshuchauhan19705@gmail.com>
2026-01-15 11:06:58 +0530

    Method - 2 (efficient method) Added

--- prime.java
+++ prime.java
@@ -8,12 +8,32 @@ public class prime {
         System.out.print("Enter Number : ");
         num = sc.nextInt();
 
+        // METHOD - 1 (time taken for large values : (n-2))
+        // if (num < 2) {
+        // isPrime = false;
+        // } else if (num == 2) {
+        // isPrime = true;
+        // } else {
+        // for (int i = 2; i <= num - 1; i++) {
+        // if (num % i == 0) {
+        // isPrime = false;
+        // }
+        // }
+        // }
+
+        // if (isPrime == true) {
+        // System.out.println(num + " is a prime number.");
+        // } else {
+        // System.out.println(num + " is not a prime number");
+        // }
+
+        // METHOD - 2 (time taken for large values : sqrt(n) which is [sqrt(n) < (n-2)])
         if (num < 2) {
             isPrime = false;
         } else if (num == 2) {
             isPrime = true;
         } else {
-            for (int i = 2; i <= num - 1; i++) {
+            for (int i = 2; i <= Math.sqrt(num); i++) { // less time taken for large values (so efficient condition)
                 if (num % i == 0) {
                     isPrime = false;
                 }
